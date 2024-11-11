ed9e3df0cbab2f3c1f268827f4eecbecab9ad041
vasavip111 <vasavi.patnaik@thebluespire.com>
2024-11-11 12:58:11 +0530

    second commit

--- test/app.test.js
+++ test/app.test.js
@@ -3,8 +3,16 @@ describe("Sample tests", () =>{
         expect(1+1).toBe(2);
     });
 
-
     test("Second test case", () =>{
         expect(1+2).toBe(3);
     });
+
+    test("Third test case", () =>{
+        expect(12+2).toBe(14);
+    });
+
+    test("Fourth test case", () =>{
+        expect(8+5).toBe(13);
+    });
+
 })
